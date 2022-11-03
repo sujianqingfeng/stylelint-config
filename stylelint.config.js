76c38e437f63ff0a6b4e3a5250f433543728c3e1
sujianqingfeng <121116111@qq.com>
2022-11-03 14:22:46 +0800

    feat: add less

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-standard-scss', 'stylelint-config-recommended-vue'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-scss', 
+    'stylelint-config-recommended-vue',
+    'stylelint-config-recommended-less'
+  ],
   rules: {
-  }
+  },
 }
